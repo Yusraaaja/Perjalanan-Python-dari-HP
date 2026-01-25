d73f31a1a5a1403b3e37f99a38b2312b7f194635
Yusraaaja <traintobusan50@gmail.com>
2026-01-26 00:14:30 +0700

    Add greeting program for aspiring engineers

---
+++ 01_logika_dasar.py
@@ -0,0 +1,9 @@
+# Program Menyapa Calon Engineer
+nama = input ("Masukkan namamu : ")
+print("Halo " + nama + ", Selamat Datang di Tangga Evolusi 1!")
+
+target_laptop = 3500000
+target_sekarang = int(input("Sudah menabung berapa hari ini? Rp "))
+
+sisa = target_laptop - target_sekarang
+print("Semangat sisa tabungan untuk laptop thinkpad t480: Rp ", sisa)
