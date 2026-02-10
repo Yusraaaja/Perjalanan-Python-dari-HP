fc2854cc4cc4d161a05ffdf07c49b0adf0141929
Muhamad Yusra Agam <traintobusan50@gmail.com>
2026-02-10 09:13:17 +0700

    Update jembatan.py

--- jembatan.py
+++ jembatan.py
@@ -7,24 +7,25 @@ def proses_data_custom():
 
         data_bersih = []
         for baris in baris_data:
-            #memisahkan berdasarkan tanda '|'
+            # Memisahkan berdasarkan tanda '|'
             bagian = baris.split('|')
-            #mengambil nilai setelah tanda ':' dan menghapus spasi
+            # Mengambil nilai setelah tanda ':' dan menghapus spasi
             nama = bagian[0].split(':')[1].strip()
             jabatan = bagian[0].split(':')[1].strip()
             umur = bagian[0].split(':')[1].strip()
 
             data_bersih.append({'Nama': nama, 'Jabatan': jabatan, 'Umur': umur})
 
-        #2. ubah menjadi dataframe
+        # 2. Ubah menjadi dataframe
         df = pd.DataFrame(data_bersih)
 
-        #3. simpan ke excel (karena tidak ada gaji, kita simpan data yang ada saja)
+        # 3. Simpan ke excel (karena tidak ada gaji, kita simpan data yang ada saja)
         df.to_excel('Laporan_final_pegawai.xlsx', index=False)
         print("Script berhasil membedah isi database_pegawai.txt!")
         print(df)
 
     except Exception as e:
         print(f"Ada masalah lain: {e}")
+
 
 proses_data_custom()
